2d85a08c3e47467de07ed727432f9938d2f091f2
f123ly <f123ly@outlook.com>
2024-07-29 01:06:21 +0800

    Create refresh-icons.bat

---
+++ refresh-icons.bat
@@ -0,0 +1,27 @@
+@echo off
+rem 检测是否为管理员权限，如果不是，则请求管理员权限
+Net session >nul 2>&1 || mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0","","runas",1)(window.close)&&exit
+
+rem 关闭 Windows 资源管理器
+taskkill /f /im explorer.exe
+
+rem 删除图标缓存数据库
+attrib -h -s -r "%userprofile%\AppData\Local\IconCache.db"
+del /f "%userprofile%\AppData\Local\IconCache.db"
+
+rem 清理系统图标缓存
+attrib /s /d -h -s -r "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\*"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_32.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_96.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_102.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_256.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_1024.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_idx.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_sr.db"
+
+rem 清理系统托盘记忆的图标
+echo y|reg delete "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v IconStreams
+echo y|reg delete "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v PastIconsStream
+
+rem 重启 Windows 资源管理器
+start explorer
